e344f6166f447b86a34a0df6128a6a51b17edc52
Mike Isman <mikeisman@gmail.com>
2025-06-30 21:45:50 -0400

    Use includes

--- rails/app/graphql/queries/fetch_players.rb
+++ rails/app/graphql/queries/fetch_players.rb
@@ -51,7 +51,7 @@ module Queries
         type [Types::PlayerType], null: false
         def resolve
             season_id = Season.current.id
-            Player.with_stats_or_current_contract(season_id)
+            Player.with_stats_or_current_contract(season_id).includes(contract: [:first_season, :last_season])
         end
     end
 end
