c32a190238e550308bc94e9b5c1ebff379afe8f4
Jamie Pinkham <me@jamiepinkham.com>
2026-01-05 05:02:45 -0500

    claude knocks out my todo list

--- rails/app/controllers/passwords_controller.rb
+++ rails/app/controllers/passwords_controller.rb
@@ -1,30 +1,38 @@
 class PasswordsController < Devise::PasswordsController
   respond_to :json
-  before_action :process_token
+  before_action :process_token, only: [:update]
 
   # PUT /resource/password
   def update
-    user = User.find(@current_user_id)
-    password = params[:new_pass]
-    if user.valid_password?(password)
-      user.password = params[:new_pass]
-      begin
-        current_user.save
-        render json: { status: "ok" }
-      rescue
-        render json: { errors: $!}
+    # Handle password reset with token (from email link)
+    if params[:user] && params[:user][:reset_password_token].present?
+      super
+    # Handle authenticated user changing their password
+    elsif @current_user_id
+      user = User.find(@current_user_id)
+      password = params[:new_pass]
+      if user.valid_password?(password)
+        user.password = params[:new_pass]
+        begin
+          user.save
+          render json: { status: "ok" }
+        rescue
+          render json: { errors: $!}
+        end
+      else
+        render json: {errors: "invalid password" }
       end
     else
-      render json: {errors: "invalid password" }
+      render json: { errors: "Unauthorized" }, status: :unauthorized
     end
   end
 
-  private 
+  private
   def process_token
     if request.headers['Authorization'].present?
       begin
         jwt = request.headers['Authorization'].split(' ')[1].remove('"')
-        jwt_payloads = JWT.decode(jwt, 
+        jwt_payloads = JWT.decode(jwt,
           'faba5c848cf90f9bd2d09dd996c76f0912cc775b1d1e460413fd235a0d7cd411f2f07352acd38408df14c7967fa3d893b8ac8d9b15b4f0860359b63847419c04')
         jwt_payload = jwt_payloads.first
         @current_user_id = jwt_payload['sub']
