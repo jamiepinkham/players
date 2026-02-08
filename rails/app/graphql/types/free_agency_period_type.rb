c56ab2419096b4ba10f22c3d8ba76bb56a68cd2c
Jamie Pinkham <me@jamiepinkham.com>
2026-02-08 12:16:31 -0500

    fix leading bids

--- rails/app/graphql/types/free_agency_period_type.rb
+++ rails/app/graphql/types/free_agency_period_type.rb
@@ -5,18 +5,18 @@ module Types
     field :bids, [Types::BidType], null: true do
       argument :team_id, ID, required: false
       argument :active, Boolean, required: false, default_value: false
+      argument :leading, Boolean, required: false
     end
     field :start_date, GraphQL::Types::ISO8601DateTime, null: true
     field :end_date, GraphQL::Types::ISO8601DateTime, null: true
     field :max_contract_length, Integer, null: false
     field :max_bids_for_team, Integer, null: false
 
-    def bids(team_id: nil, active:)
-      if team_id
-        Bid.where(free_agency_period_id: object.id, team_id: team_id, is_active: active)
-      else
-        Bid.where(free_agency_period_id: object.id, is_active: active)
-      end
+    def bids(team_id: nil, active:, leading: nil)
+      query = Bid.where(free_agency_period_id: object.id, is_active: active)
+      query = query.where(team_id: team_id) if team_id
+      query = query.where(is_leading: leading) if !leading.nil?
+      query
     end
 
   end
